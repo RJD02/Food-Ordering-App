8983a824738c0071aff61fcf41792974aa1e7f74
Raviraj Dulange <redhood.dumb@gmail.com>
2021-07-23 08:45:33 +0530

    Update components to contain header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
 import classes from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg";
+import HeaderCardButton from "./HeaderCardButton";
 
 export default function Header() {
   return (
     <React.Fragment>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <button>Cart</button>
+        <HeaderCardButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="Meals on table" />
