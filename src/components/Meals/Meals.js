6fbc1efa4182c4115953a3a3846271a4af8b1e2d
Raviraj Dulange <redhood.dumb@gmail.com>
2021-07-23 14:04:54 +0530

    Add Meals component

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+import MealsSummary from "./MealsSummary";
+
+export default function Meals() {
+  return (
+    <Fragment>
+      <MealsSummary />
+    </Fragment>
+  );
+}
