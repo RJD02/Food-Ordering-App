44b5ab6c65dd7c7442b588041fe0026447a6b860
Raviraj Dulange <redhood.dumb@gmail.com>
2021-07-27 20:02:36 +0530

    Branch out to Input and MealForm component

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -1,11 +1,20 @@
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
 export default function MealItem(props) {
+  const price = `$${props.price.toFixed(2)}`;
   return (
-    <li className={classes.meal}>
-      <h3>{props.name}</h3>
-      <p className={classes.description}>{props.description}</p>
-      <p className={classes.price}>{props.price}</p>
+    <li className={classes.meal} key={props.id}>
+      <div>
+        <h3>{props.name}</h3>
+        <div>
+          <p className={classes.description}>{props.description}</p>
+        </div>
+        <p className={classes.price}>{price}</p>
+      </div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 }
