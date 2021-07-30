1f439d048a0a7bb579523ad16905b598d035a58c
Raviraj Dulange <redhood.dumb@gmail.com>
2021-07-30 15:34:51 +0530

    Pass props onClose prop to HeaderCardButton component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,12 +3,12 @@ import classes from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg";
 import HeaderCardButton from "./HeaderCardButton";
 
-export default function Header() {
+export default function Header(props) {
   return (
     <React.Fragment>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <HeaderCardButton />
+        <HeaderCardButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="Meals on table" />
