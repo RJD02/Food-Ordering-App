ae75ebff8b5559f8b3e8083250d3661fc2236225
Raviraj Dulange <redhood.dumb@gmail.com>
2021-07-30 15:34:02 +0530

    Pass onHideCart handler to Modal component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,7 +2,7 @@ import React from "react";
 import { v4 as uuidv4 } from "uuid";
 import classes from "./Cart.module.css";
 import Modal from "./Modal";
-export default function Cart() {
+export default function Cart(props) {
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {[{ id: uuidv4(), name: "Faluda", amount: 2, price: 2 }].map((item) => (
@@ -11,14 +11,16 @@ export default function Cart() {
     </ul>
   );
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.23</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
