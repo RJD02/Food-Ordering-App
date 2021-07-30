c366473cb5dff784be87c0a454cd571fc706456d
Raviraj Dulange <redhood.dumb@gmail.com>
2021-07-30 18:52:13 +0530

    Add logic to get number of items in cart

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -4,6 +4,9 @@ import classes from "./HeaderCardButton.module.css";
 import CartContext from "../../store/Cart-Context";
 export default function HeaderCardButton(props) {
   const ctx = useContext(CartContext);
+  const numberOfCartItems = ctx.items.reduce((currNumber, item) => {
+    return currNumber + item.amount;
+  }, 0);
   return (
     <Fragment>
       <button className={classes.button} onClick={props.onClick}>
