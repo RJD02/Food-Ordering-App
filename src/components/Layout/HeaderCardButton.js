7a7c9bac6db176dc95ab194a31f37880b452fdfe
Raviraj Dulange <redhood.dumb@gmail.com>
2021-07-30 15:35:23 +0530

    Add onClick prop to button element to hide the modal

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -5,7 +5,7 @@ import classes from "./HeaderCardButton.module.css";
 export default function HeaderCardButton(props) {
   return (
     <Fragment>
-      <button className={classes.button}>
+      <button className={classes.button} onClick={props.onClick}>
         <span className={classes.icon}>
           <CartIcon />
         </span>
