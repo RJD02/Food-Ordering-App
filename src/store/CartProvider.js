53212745298512e3eaa3e01d31830076fc0f9bda
Raviraj Dulange <redhood.dumb@gmail.com>
2021-07-30 18:52:38 +0530

    Use reducer hook to manage state and add default code

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useReducer } from "react";
 import CartContext from "./Cart-Context";
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.amount * action.item.price;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
+
 export default function CartProvider(props) {
-  const addItemToCartHandler = (item) => {};
-  const removeItemFromCartHandler = (id) => {};
+  const [cartState, cartDispatch] = useReducer(cartReducer, defaultCartState);
+  const addItemToCartHandler = (item) => {
+    cartDispatch({
+      type: "ADD",
+      item,
+    });
+  };
+  const removeItemFromCartHandler = (id) => {
+    cartDispatch({
+      type: "REMOVE",
+      id,
+    });
+  };
   const cartContext = {
     items: [],
     totalAmount: 0,
