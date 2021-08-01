b8f9c67606063d3075a544485d2d0a506375a325
Raviraj Dulange <redhood.dumb@gmail.com>
2021-08-02 02:36:56 +0530

    Add cartState as the default value to cartContext

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -34,8 +34,8 @@ export default function CartProvider(props) {
     });
   };
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
