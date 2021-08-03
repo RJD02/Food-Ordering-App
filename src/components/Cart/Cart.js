34287fae201534ae004e220131adc436dd02e09b
Raviraj Dulange <redhood.dumb@gmail.com>
2021-08-03 12:52:58 +0530

    Display items present in context instead of dummy data

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,14 @@
-import React from "react";
-import { v4 as uuidv4 } from "uuid";
+import React, { useContext } from "react";
+import CartContext from "../../store/Cart-Context";
 import classes from "./Cart.module.css";
 import Modal from "./Modal";
 export default function Cart(props) {
+  const ctx = useContext(CartContext);
+  const totalAmount = `$${ctx.totalAmount.toFixed(2)}`;
+  const hasItems = ctx.items.length > 0;
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: uuidv4(), name: "Faluda", amount: 2, price: 2 }].map((item) => (
+      {ctx.items.map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
@@ -15,13 +18,13 @@ export default function Cart(props) {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.23</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
