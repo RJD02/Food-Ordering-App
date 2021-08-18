4df8c6ccd434b1e24b84eb21be9ad01bd17894c2
Raviraj Dulange <redhood.dumb@gmail.com>
2021-08-18 19:39:34 +0530

    Add Meals component to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Fragment } from "react";
 import Header from "./components/Layout/Header";
-
+import Meals from "./components/Meals/Meals";
 function App() {
   return (
     <Fragment>
       <Header />
+      <Meals />
     </Fragment>
   );
 }
