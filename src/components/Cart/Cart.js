3e57ca17ccdd4f27c17747d7d3f7633b5cbbbcfd
Raviraj Dulange <redhood.dumb@gmail.com>
2021-08-20 16:08:11 +0530

    Make close Button in cart component work

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -20,7 +20,9 @@ const Cart = (props) => {
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Close</button>
+        <button onClick={props.onHideCart} className={classes["button--alt"]}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
