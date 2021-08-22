6726bcb20386a4d6788498a6da0655db90ff6138
Raviraj Dulange <redhood.dumb@gmail.com>
2021-08-23 00:59:04 +0530

    Add animation logic for cart header button

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,6 +1,7 @@
 import classes from "./CartItem.module.css";
 
 const CartItem = (props) => {
+  //   console.log(props.price);
   const price = `$${props.price.toFixed(2)}`;
 
   return (
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,35 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
   const cartCtx = useContext(CartContext);
+
   const numOfCartItems = cartCtx.items.reduce((total, curr) => {
     return total + curr.amount;
   }, 0);
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) return;
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartCtx.items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
