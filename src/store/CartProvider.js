c485afaf4f354d6b3a00c4b08e0d0eb989185800
Raviraj Dulange <redhood.dumb@gmail.com>
2021-08-22 21:02:04 +0530

    Extract context maintaining logic into its own file and add addItem logic

---
+++ src/store/CartProvider.js
@@ -0,0 +1,40 @@
+import React, { useReducer } from "react";
+import CartContext from "./cart-context";
+
+const defaultCart = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+  }
+  return defaultCart;
+};
+
+export default function CartProvider(props) {
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCart);
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({
+      type: "ADD",
+      item: item,
+    });
+  };
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({
+      type: "REMOVE",
+      id: id,
+    });
+  };
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+}
