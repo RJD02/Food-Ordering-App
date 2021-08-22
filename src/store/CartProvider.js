043156f7b736ce3e34e886c04927d01b5f72923b
Raviraj Dulange <redhood.dumb@gmail.com>
2021-08-22 21:28:39 +0530

    Add CartItem component with styles

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -11,6 +11,7 @@ const cartReducer = (state, action) => {
     const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
