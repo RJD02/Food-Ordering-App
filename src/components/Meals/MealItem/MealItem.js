3377dc7db321eef9a1a2a905fba0bbcd8231b539
Raviraj Dulange <redhood.dumb@gmail.com>
2021-08-22 21:19:20 +0530

    Structure the code to return item in preferred format

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,20 @@
-import MealItemForm from './MealItemForm';
-import classes from './MealItem.module.css';
+import MealItemForm from "./MealItemForm";
+import classes from "./MealItem.module.css";
+import { useContext } from "react";
+import CartContext from "../../../store/cart-context";
 
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
+  const cartCtx = useContext(CartContext);
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      amount: amount,
+      id: props.id,
+      name: props.name,
+      price: props.price,
+    });
+  };
 
   return (
     <li className={classes.meal}>
@@ -12,7 +24,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id} />
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
